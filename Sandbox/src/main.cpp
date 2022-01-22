31cf4b6e272abe184af34037b04f488b61956d69
Samdo <s4mattsson@gmail.com>
2022-01-22 12:26:25 +0100

    Updated Sandbox.

--- Sandbox/src/main.cpp
+++ Sandbox/src/main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
 
+#include <Peio/Networking/Init.h>
+#include <Peio/Networking/Sockets.h>
+
+struct NetHandler : public Peio::EventHandler<Peio::Net::AcceptEvent, Peio::Net::ReceiveEvent> {
+
+
+
+};
+
 int main() {
-	std::cout << "Hello world!" << std::endl;
+	
+	try {
+
+		Peio::Net::Init();
+
+		Peio::Net::ListenerSocket listener;
+		listener.Init();
+
+		Peio::Net::Hint hint;
+		hint.Init(14000);
+		listener.Bind(hint);
+
+		listener.RegisterEvents();
+
+		WSACleanup();
+
+	}
+	catch (Peio::Net::Exception exception) {
+		std::cout << "Net exception: " << exception.msg << " in " << exception.file
+			<< " at line " << exception.line << ". WSAError: " << exception.wsaError << std::endl;
+	}
+	catch (Peio::Exception exception) {
+		std::cout << "Exception: " << exception.msg << " " << exception.file
+			<< " at line " << exception.line << "." << std::endl;
+	}
+
 	return 0;
 }
