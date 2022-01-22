e97766fe63e26bfb65c5d4dfcd9d97b1a5e7324d
Samdo <s4mattsson@gmail.com>
2022-01-22 11:41:29 +0100

    Added Socket.h

--- src/Peio/Networking/Init.cpp
+++ src/Peio/Networking/Init.cpp
@@ -1,10 +1,14 @@
 #define PEIO_NET_EXPORTING
 #include "Init.h"
 
-void Peio::NET::Init()
-{
-	WSADATA data = {};
-	if (WSAStartup(MAKEWORD(2, 2), &data) != 0) {
-		throw PEIO_NET_EXCEPTION("Failed to initialize Winsock.");
+namespace Peio::NET {
+
+	void Init()
+	{
+		WSADATA data = {};
+		if (WSAStartup(MAKEWORD(2, 2), &data) != 0) {
+			throw PEIO_NET_EXCEPTION("Failed to initialize Winsock.");
+		}
 	}
+
 }
