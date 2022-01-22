3a9d1027d7f296f6bfad4af5c3a62393aaf16037
Samdo <s4mattsson@gmail.com>
2022-01-22 11:54:02 +0100

    Added Hint.

---
+++ src/Peio/Networking/Hint.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Exception.h"
+
+#include <string>
+
+namespace Peio::NET {
+
+	struct PEIO_NET_EXPORT Hint {
+
+		void Init(ushort port, const char* ip = nullptr);
+		void Init(ushort port, const uchar* ip);
+
+		_NODISCARD std::string GetIp() const;
+		_NODISCARD ushort GetPort() const noexcept;
+
+		_NODISCARD sockaddr* GetSockaddr() noexcept;
+		_NODISCARD const sockaddr* GetSockaddr() const noexcept;
+
+		sockaddr_in hint = {};
+
+	};
+
+}
